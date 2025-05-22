4148f7fe9acb73f08c7931736fc52a52633002cd
juliaaquinoo <julia.lima23@etepd.com>
2025-05-22 12:36:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app)
-
+CORS(app) 
 @app.route('/calcular', methods=['POST'])
 def calcular():
     data = request.get_json()
     expressao = data.get('expressao', '')
-
     try:
-        # Corrigir divisão e operadores visuais para Python
-        expressao = expressao.replace('÷', '/').replace('×', '*').replace('%', '/100')
+        expressao = expressao.replace('×', '*').replace('÷', '/')
         resultado = eval(expressao)
         return jsonify({'resultado': str(resultado)})
-    except Exception as e:
+    except:
         return jsonify({'resultado': 'Erro'})
-
 if __name__ == '__main__':
     app.run(debug=True)
